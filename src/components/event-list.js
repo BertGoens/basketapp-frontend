83b1632f90252f1ec126de548c54e31c4c2a6328
Bert Goens <bertgoens@gmail.com>
2017-08-27 17:31:56 +0200

    make event list work

--- src/components/event-list.js
+++ src/components/event-list.js
@@ -1,111 +1,93 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
 import moment from 'moment'
+import { ErrorSummary } from './error-summary'
 
-export class EventList extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      events: null,
-      lastMonth: null
-    }
-  }
-
-  GetEventList() {
-    return fetch('/events?results=20')
-      .then((data) => {
-        // TODO work with cache
-        this.setState({ events: data.results })
-      })
-  }
-
-  componentDidMount() {
-    this.GetEventList()
-  }
-
-  render() {
-    if (this.state.events != null) {
-      const eventList = this.state.events.map((item, i) => {
-        console.dir(item)
-        const startDateTime = item.event_start_datetime;
-
-        const startTime = moment(startDateTime).format('HH:mm')
-        const dayNumber = moment(startDateTime).format('D')
-        const dayName = moment(startDateTime).format('ddd')
-
-        const monthName = moment(item.dataValues.event_start_datetime).format('MMMM')
-        if (monthName !== this.state.lastMonth) {
-          return (
-            <div>
-              <EventMonth />
-              < Event event={item} dayNumber={dayNumber} dayShortName={dayName} startTime={startTime} />
-            </div>
-          )
-        } else {
-          return (
-            < Event event={item} dayNumber={dayNumber} dayShortName={dayName} startTime={startTime} />
-          )
-        }
-      })
-
-      return eventList
-    } else {
-      return null
-    }
-  }
+const failEvent = {
+  event_start_datetime: new Date(),
+  title: 'Title',
+  location: 'Location',
+  eventStatus: 'unknown'
 }
 
-const Event = (props) => {
+export const Event = ({ onChange, errors, event, lastMonth }) => {
+  console.dir(event)
+
+  if (!event) {
+    // fall back
+    event = failEvent
+  }
+
+  const startDateTime = event.event_start_datetime;
+
+  const startTime = moment(startDateTime).format('HH:mm')
+  const dayNumber = moment(startDateTime).format('D')
+  const dayName = moment(startDateTime).format('ddd')
+
+  const monthName = moment(event.event_start_datetime).format('MMMM')
+
+  return (
+    <div>
+      {errors.summary && <ErrorSummary errorSummary={errors.summary} />}
+      {monthName !== lastMonth && <EventMonth monthName={monthName} />}
+      < EventDisplay
+        event={event}
+        dayNumber={dayNumber}
+        dayShortName={dayName}
+        startTime={startTime}
+      />
+    </div>
+  )
+}
+
+Event.PropTypes = {
+  onChange: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired,
+  event: PropTypes.object.isRequired,
+  lastMonth: PropTypes.string.isRequired
+}
+
+const EventDisplay = ({ event, dayNumber, dayShortName, startTime }) => {
   return (
     <div >
       <EventDivider />
 
-      <div key={props.event.id} className="row event-row">
+      <div key={event.id} className="row event-row">
         <div className="event-date col s3">
-          <p className="left-align">{props.dayNumber}</p>
-          <p className="center-align event-date-month">{props.dayShortName}</p>
-          <p className="center-align">{props.startTime}</p>
+          <p className="left-align">{dayNumber}</p>
+          <p className="center-align event-date-month">{dayShortName}</p>
+          <p className="center-align">{startTime}</p>
         </div>
         <div className="col s7 event-brief">
-          <p className="event-title truncate">{props.event.Title}</p>
-          <p className="event-location truncate">{props.event.location}</p>
+          <p className="event-title truncate">{event.title}</p>
+          <p className="event-location truncate">{event.location}</p>
         </div>
         <div className="col s2 event-status">
           <span className="event-status-center-helper"></span>
-          <EventStatus event={props.event} />
+          <EventStatus event={event.eventStatus} />
         </div>
       </div>
     </div >
   )
 }
 
-const EventStatus = (props) => {
-  const ok = <img className="event-status-image" src="/assets/img/event-status/StatusOk64x64.png" alt="OK" height="26" width="26"></img>
-  const notOk = <img className="event-status-image" src="/assets/img/event-status/StatusNotOk64x64.png" alt="OK" height="26" width="26"></img>
-  const unknown = <img className="event-status-image" src="/assets/img/event-status/StatusUnknown64x64.png" alt="OK" height="26" width="26"></img>
-
-  let result
-  switch (props.event.status) {
-    case 'ok':
-      result = ok
-      break;
-    case 'notOk':
-      result = notOk
-      break;
-    case 'unknown':
-      result = unknown
-      break;
-    default:
-      result = unknown
-      break;
+const EventStatus = ({ eventStatus }) => {
+  if (eventStatus === 'ok') {
+    return <img className="event-status-image" src="/assets/img/event-status/StatusOk64x64.png" alt="OK" height="26" width="26"></img>
+  } else if (eventStatus === 'notOk') {
+    return <img className="event-status-image" src="/assets/img/event-status/StatusNotOk64x64.png" alt="OK" height="26" width="26"></img>
+  } else {
+    return <img className="event-status-image" src="/assets/img/event-status/StatusUnknown64x64.png" alt="OK" height="26" width="26"></img>
   }
+}
 
-  return (
-    { result }
-  )
+EventStatus.PropTypes = {
+  eventStatus: PropTypes.string.isRequired
 }
 
 // eslint-disable-next-line
-const EventStatusActions = (props) => {
+const EventStatusActions = () => {
   return (
     <div className="event-status-actions">
       <div className="col s3">
@@ -121,17 +103,21 @@ const EventStatusActions = (props) => {
   )
 }
 
-const EventDivider = (props) => {
+const EventDivider = () => {
   return (
     <div className="divider"></div>
   )
 }
 
-const EventMonth = (props) => {
+const EventMonth = ({ monthName }) => {
   return (
     <div>
       <EventDivider />
-      <p className="center-align event-month light-blue-text text-accent-4">{props.monthName}</p>
+      <p className="center-align event-month light-blue-text text-accent-4">{monthName}</p>
     </div>
   )
+}
+
+EventMonth.PropTypes = {
+  monthName: PropTypes.string.isRequired
 }
--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -2,13 +2,81 @@ import '../../components/events.css'
 
 import React from 'react';
 
-import { EventList } from '../../components/event-list'
+import { Event } from '../../components/event-list'
 
-export const HomePage = (props) => {
-  return (
-    <div>
-      <h1>Under Maintenance</h1>
-      <EventList />
-    </div>
-  )
+export class HomePage extends React.Component {
+  /**
+   * Class constructor.
+   */
+  constructor(props) {
+    super(props);
+
+    // set the initial component state
+    this.state = {
+      errors: {},
+      events: {},
+      lastMonth: null,
+    };
+
+    this.getEventList = this.getEventList.bind(this);
+    this.postEventStatusUpdate = this.postEventStatusUpdate.bind(this);
+  }
+
+  postEventStatusUpdate(eventId, status) {
+    console.log(`Update ${eventId} to ${status}`)
+  }
+
+  /**
+   * Get the events
+   */
+  getEventList() {
+    let myInit = {
+      method: 'GET',
+      credentials: 'include', //pass cookies, for authentication
+      headers: new Headers({
+        'Accept': 'application/json'
+      }),
+    };
+
+    let url = '/api/events?results=20'
+
+    let myRequest = new Request(url, myInit);
+
+    fetch(myRequest)
+      .then((data) => {
+        data.json()
+      })
+      // Change component state
+      .then((data) => {
+        // TODO work with cache
+        this.setState({ events: data.results })
+      }).catch((error) => {
+        if (!error.summary) {
+          error.summary = '🚧 Server unavailable. 🚧'
+        }
+        this.setState({
+          errors: error
+        })
+      })
+  }
+
+  componentDidMount() {
+    this.getEventList()
+  }
+
+  /**
+   * Render the component.
+   */
+  render() {
+    return (
+      <div>
+        <Event
+          onChange={this.postEventStatusUpdate}
+          errors={this.state.errors}
+          event={this.state.events[0]}
+          lastMonth={this.state.lastMonth}
+        />
+      </div>
+    );
+  }
 }
